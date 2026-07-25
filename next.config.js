8f841643acf328060f3e3cc6d6f7d211d4f9bfec
tanzid-48 <mdtanzid.525@gmail.com>
2026-07-25 13:41:49 +0600

    fix project slugs

--- next.config.js
+++ next.config.js
@@ -2,9 +2,11 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "i.ibb.co.com" },
-      { protocol: "https", hostname: "i.ibb.co" },
-      { protocol: "https", hostname: "images.unsplash.com" },
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+      },
     ],
   },
 };
